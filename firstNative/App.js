eb7a52318fca0e20e8cab92a15522da9fae993d8
vaibhav <borkarv908@gmail.com>
2026-03-04 23:59:40 +0530

    making first goal/tasl  app

--- firstNative/App.js
+++ firstNative/App.js
@@ -1,6 +1,16 @@
+import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+  function goalInputHandler(enteredText) {
+    setEnteredGoal(enteredText);
+  }
+  function goalAddHandler() {
+    // setCourseGoals({ ...courseGoals, enteredGoal });
+    setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]);
+  }
   return (
     <View style={styles.appConatainer}>
       {/* <View>
@@ -14,13 +24,57 @@ export default function App() {
       <Text style={styles.japanText}>welcome to japan</Text>
       <Button title="tap me" /> */}
 
-      <View>
-        <TextInput placeholder="Enter your goals" />
-        <Button title="Add goal" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Enter your goals"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add goal" onPress={goalAddHandler} />
       </View>
-      <View>
-        <Text>List of goals...</Text>
+      <View style={styles.goalShow}>
+        {courseGoals.map((goal) => (
+          <Text key={goal}>{goal}</Text>
+        ))}
       </View>
+
+      {/* flexbox */}
+      {/* <View
+        style={{
+          backgroundColor: "red",
+          // height: 100,
+          // width: 100,
+          justifyContent: "center",
+          alignItems: "center",
+          flex:1
+        }}
+      >
+        <Text>1</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: "blue",
+          // height: 100,
+          // width: 100,
+          flex:2,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: "green",
+          // height: 100,
+          // width: 100,
+          flex:3,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>3</Text>
+      </View> */}
     </View>
   );
 }
@@ -28,7 +82,64 @@ export default function App() {
 const styles = StyleSheet.create({
   //stylesheet object
   appConatainer: {
+    flex: 1,
     padding: 50,
-    margin: 1,
+    paddingHorizontal: 16,
+  },
+  // appConatainer: {
+  //   padding: 50,
+  //   width: "80%",
+  //   height: 300,
+  //   flexDirection: "row",
+  //   justifyContent: "space-between",
+  //   alignItems: "center",
+  // },
+  flexContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    height: 80,
+    width: 80,
+  },
+  textInput1: {
+    borderColor: "red",
+    borderWidth: 2,
+    height: 50,
+    width: 50,
+    textAlign: "center",
+  },
+  textInput2: {
+    borderColor: "blue",
+    borderWidth: 2,
+    height: 50,
+    width: 50,
+    textAlign: "center",
+  },
+  textInput3: {
+    borderColor: "green",
+    borderWidth: 2,
+    height: 50,
+    width: 50,
+    textAlign: "center",
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBlockColor: "black  ",
+    borderBottomWidth: 2,
+    marginBottom: 2,
+  },
+  textInput: {
+    width: "70%",
+    borderWidth: 2,
+    borderColor: "#cb5252cc",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalShow: {
+    flex: 6,
+    marginTop: 20,
   },
 });
