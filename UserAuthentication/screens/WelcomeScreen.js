f2b835f89513e4c11e18c920a36ab264b2550a4b
vaibhav <borkarv908@gmail.com>
2026-04-22 13:43:26 +0530

    access data only by authenticated users from firebase

--- UserAuthentication/screens/WelcomeScreen.js
+++ UserAuthentication/screens/WelcomeScreen.js
@@ -1,10 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native';
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function WelcomeScreen() {
+  const [msg, setMsg] = useState("");
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://expense-eeadc-default-rtdb.firebaseio.com/message.json?auth=${token}`,
+      )
+      .then((response) => setMsg(response.data));
+  }, [token]);
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{msg}</Text>
     </View>
   );
 }
@@ -14,13 +29,13 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 32,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
   },
 });
